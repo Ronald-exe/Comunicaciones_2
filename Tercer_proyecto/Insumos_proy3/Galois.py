38979ef24b28f9f7a6c7c77e752f1c75a3d2f0e5
Leo1307 <leoperezsandoval@estudiantec.com>
2025-10-14 23:45:06 -0600

    Galois

---
+++ Tercer_proyecto/Insumos_proy3/Galois.py
@@ -0,0 +1,18 @@
+import numpy as np
+import galois
+
+GF = galois.GF(2**4, irreducible_poly=galois.Poly.Int(0b10011))
+
+# Tabla de suma
+add_table = np.zeros((16,16), dtype=int)
+mul_table = np.zeros((16,16), dtype=int)
+
+for i in range(16):
+    for j in range(16):
+        add_table[i,j] = int(GF(i) + GF(j))
+        mul_table[i,j] = int(GF(i) * GF(j))
+
+print("Tabla de Suma GF(16):")
+print(add_table)
+print("\nTabla de Multiplicación GF(16):")
+print(mul_table)
